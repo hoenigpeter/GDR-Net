197df568bc94320c18dd2a898f1668f80b72002e
Peter Hoenig <hpfreak1004@gmail.com>
2023-09-22 17:23:00 +0200

    bash file for no aug training

---
+++ train_lmo_random_no_aug.bash
@@ -0,0 +1,8 @@
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo_random_texture_all_no_aug/a6_cPnP_lmo_random_texture_all_no_aug_ape.py 0
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo_random_texture_all_no_aug/a6_cPnP_lmo_random_texture_all_no_aug_can.py 0
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo_random_texture_all_no_aug/a6_cPnP_lmo_random_texture_all_no_aug_cat.py 0
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo_random_texture_all_no_aug/a6_cPnP_lmo_random_texture_all_no_aug_driller.py 0
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo_random_texture_all_no_aug/a6_cPnP_lmo_random_texture_all_no_aug_duck.py 0
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo_random_texture_all_no_aug/a6_cPnP_lmo_random_texture_all_no_aug_eggbox.py 0
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo_random_texture_all_no_aug/a6_cPnP_lmo_random_texture_all_no_aug_glue.py 0
+./core/gdrn_modeling/train_gdrn.sh configs/gdrn/lmo_random_texture_all_no_aug/a6_cPnP_lmo_random_texture_all_no_aug_holepuncher.py 0
